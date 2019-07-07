8ad6927aae7ca6984808094a7055f67837aba7fc
Anthony Shaw <anthonyshaw@apache.org>
2019-07-08 09:17:29 +1000

    Project layout

---
+++ README.rst
@@ -0,0 +1,4 @@
+pytest-nunit
+============
+
+A pytest plugin for generating Nunit3 test result XML output
---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+from pytest_nunit import __version__
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+def getversion():
+    if 'BUILD_VERSION' in os.environ:
+        return os.environ['BUILD_VERSION']
+    else:
+        return __version__
+
+setup(
+    name='pytest-nunit',
+    version=getversion(),
+    author='Anthony Shaw',
+    author_email='anthonyshaw@apache.org',
+    maintainer='Anthony Shaw',
+    maintainer_email='anthonyshaw@apache.org',
+    license='MIT',
+    url='https://github.com/tonybaloney/pytest-nunit',
+    description='A pytest plugin for generating Nunit3 test result XML output',
+    long_description=read('README.rst'),
+    py_modules=['pytest_nunit'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['pytest>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'nunit = pytest_nunit.plugin',
+        ],
+    },
+)
