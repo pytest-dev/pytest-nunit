9491e8121bc7aa17834559a1d685711f52efd153
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-07-08 11:39:00 +1000

    Start a XSD->Attrs tool

--- generate-models.py
+++ generate-models.py
@@ -11,12 +11,39 @@ import click
 import xmlschema
 
 
+XS_ATOMIC_MAP = {
+    'xs:string': 'str',
+    'xs:int': 'object'
+}
+
+
+def make_attrib(attrib):
+    """
+    Make attrs attribute from XmlAttribute
+    :return: `str`
+    """
+    args = []
+    if isinstance(attrib.type, xmlschema.validators.XsdAtomicBuiltin):
+        args.append('type={0}'.format(XS_ATOMIC_MAP.get(attrib.type.name, 'object')))
+    return "attr.ib({0})".format(','.join(args))
+
+
 @click.command()
 @click.argument('xsd_path', type=click.Path(exists=True))
 @click.argument('output_path', type=click.Path(exists=False))
 def main(xsd_path, output_path):
     xsd = xmlschema.XMLSchema(xsd_path)
-    print(xsd)
+    out = "import attr\n\n"
+    for name, type_ in xsd.types.items():
+        out += "@attr.s\nclass {0}(object):\n".format(name)
+        if isinstance(type_, xmlschema.validators.XsdComplexType):
+            for name, attrib in type_.attributes.items():
+                out += "    {0} = {1}\n".format(name, make_attrib(attrib))
+        out += "\n\n"
+
+    print(out)
+    with open(output_path, "w") as out_f:
+        out_f.write(out)
 
 if __name__ == '__main__':
     main()
--- pytest_nunit/models.py
+++ pytest_nunit/models.py
@@ -1,5 +1,86 @@
-"""
-A reflection of the XSD described in nunit3-test-result.xsd.
+import attr
 
-Generated automatically from generate-models.py.
-"""
+@attr.s
+class TestDurationType(object):
+
+
+@attr.s
+class NonnegativeInt32(object):
+
+
+@attr.s
+class TestRunType(object):
+    id = attr.ib(type=object)
+    name = attr.ib(type=object)
+    fullname = attr.ib(type=object)
+    testcasecount = attr.ib()
+    result = attr.ib()
+    label = attr.ib(type=object)
+    start-time = attr.ib(type=object)
+    end-time = attr.ib(type=object)
+    duration = attr.ib()
+    total = attr.ib()
+    passed = attr.ib()
+    failed = attr.ib()
+    inconclusive = attr.ib()
+    skipped = attr.ib()
+    asserts = attr.ib()
+    random-seed = attr.ib(type=object)
+
+
+@attr.s
+class TestFilterType(object):
+
+
+@attr.s
+class CompositeFilterType(object):
+
+
+@attr.s
+class ValueMatchFilterType(object):
+    re = attr.ib(type=object)
+
+
+@attr.s
+class TestCaseElementType(object):
+    id = attr.ib(type=object)
+    name = attr.ib(type=object)
+    fullname = attr.ib(type=object)
+    methodname = attr.ib(type=object)
+    classname = attr.ib(type=object)
+    runstate = attr.ib()
+    seed = attr.ib(type=object)
+    result = attr.ib()
+    label = attr.ib(type=object)
+    site = attr.ib()
+    start-time = attr.ib(type=object)
+    end-time = attr.ib(type=object)
+    duration = attr.ib()
+    asserts = attr.ib()
+
+
+@attr.s
+class TestSuiteElementType(object):
+    id = attr.ib(type=object)
+    name = attr.ib(type=object)
+    fullname = attr.ib(type=object)
+    methodname = attr.ib(type=object)
+    classname = attr.ib(type=object)
+    runstate = attr.ib()
+    type = attr.ib()
+    testcasecount = attr.ib()
+    result = attr.ib()
+    label = attr.ib(type=object)
+    site = attr.ib()
+    start-time = attr.ib(type=object)
+    end-time = attr.ib(type=object)
+    duration = attr.ib()
+    asserts = attr.ib()
+    total = attr.ib()
+    passed = attr.ib()
+    failed = attr.ib()
+    warnings = attr.ib()
+    inconclusive = attr.ib()
+    skipped = attr.ib()
+
+
