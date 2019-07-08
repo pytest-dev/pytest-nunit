0fbc6751a5cd01c6f3b83962d695d29840113f7c
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-07-08 12:15:38 +1000

    Progress on the XSD model generator

--- generate-models.py
+++ generate-models.py
@@ -12,8 +12,10 @@ import xmlschema
 
 
 XS_ATOMIC_MAP = {
-    'xs:string': 'str',
-    'xs:int': 'object'
+    '{http://www.w3.org/2001/XMLSchema}string': 'str',
+    '{http://www.w3.org/2001/XMLSchema}int': 'int',
+    '{http://www.w3.org/2001/XMLSchema}boolean': 'bool',
+    '{http://www.w3.org/2001/XMLSchema}decimal': 'float'
 }
 
 
@@ -22,10 +24,27 @@ def make_attrib(attrib):
     Make attrs attribute from XmlAttribute
     :return: `str`
     """
-    args = []
+    args = ['metadata={"name": \'%s\'}' % (attrib.name)]
+
+    # Put type hints on XSD atomic types
     if isinstance(attrib.type, xmlschema.validators.XsdAtomicBuiltin):
-        args.append('type={0}'.format(XS_ATOMIC_MAP.get(attrib.type.name, 'object')))
-    return "attr.ib({0})".format(','.join(args))
+        _atomic_type = XS_ATOMIC_MAP.get(attrib.type.name, 'object')
+        args.append('type={0}'.format(_atomic_type))
+
+        if attrib.use == 'required':
+            args.append('validator=attr.validators.instance_of({0})'.format(_atomic_type))
+    elif isinstance(attrib.type, xmlschema.validators.XsdAtomicRestriction):
+        if attrib.use == 'required':
+            args.append('validator=attr.validators.instance_of({0})'.format(attrib.type.name))
+
+    # Set default value to none for optional args
+    if attrib.use == 'optional':
+        # Set all args to optional
+        args.append('default=attr.NOTHING')
+
+    name = attrib.name.replace('-', '_')
+
+    return "{0} = attr.ib({1})".format(name, ', '.join(args))
 
 
 @click.command()
@@ -35,15 +54,27 @@ def main(xsd_path, output_path):
     xsd = xmlschema.XMLSchema(xsd_path)
     out = "import attr\n\n"
     for name, type_ in xsd.types.items():
-        out += "@attr.s\nclass {0}(object):\n".format(name)
+
+        has_parts = False
+
         if isinstance(type_, xmlschema.validators.XsdComplexType):
+            out += "@attr.s\nclass {0}(object):\n".format(name)
+
             for name, attrib in type_.attributes.items():
-                out += "    {0} = {1}\n".format(name, make_attrib(attrib))
+                out += "    {0}\n".format(make_attrib(attrib))
+                has_parts = True
+        elif isinstance(type_, xmlschema.validators.XsdAtomicRestriction):
+            out += "@attr.s\nclass {0}({1}):\n".format(name, XS_ATOMIC_MAP.get(type_.base_type.name, 'object'))
+
+        if not has_parts:
+            out += "    pass\n"  # avoid having empty class
+
         out += "\n\n"
 
-    print(out)
+    # print(out)
     with open(output_path, "w") as out_f:
         out_f.write(out)
+
 
 if __name__ == '__main__':
     main()
--- pytest_nunit/models.py
+++ pytest_nunit/models.py
@@ -1,86 +1,90 @@
 import attr
 
 @attr.s
-class TestDurationType(object):
+class TestDurationType(float):
+    pass
 
 
 @attr.s
-class NonnegativeInt32(object):
+class NonnegativeInt32(int):
+    pass
 
 
 @attr.s
 class TestRunType(object):
-    id = attr.ib(type=object)
-    name = attr.ib(type=object)
-    fullname = attr.ib(type=object)
-    testcasecount = attr.ib()
-    result = attr.ib()
-    label = attr.ib(type=object)
-    start-time = attr.ib(type=object)
-    end-time = attr.ib(type=object)
-    duration = attr.ib()
-    total = attr.ib()
-    passed = attr.ib()
-    failed = attr.ib()
-    inconclusive = attr.ib()
-    skipped = attr.ib()
-    asserts = attr.ib()
-    random-seed = attr.ib(type=object)
+    id = attr.ib(metadata={"name": 'id'}, type=str, validator=attr.validators.instance_of(str))
+    name = attr.ib(metadata={"name": 'name'}, type=str, validator=attr.validators.instance_of(str))
+    fullname = attr.ib(metadata={"name": 'fullname'}, type=str, validator=attr.validators.instance_of(str))
+    testcasecount = attr.ib(metadata={"name": 'testcasecount'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    result = attr.ib(metadata={"name": 'result'}, validator=attr.validators.instance_of(None))
+    label = attr.ib(metadata={"name": 'label'}, type=str, default=attr.NOTHING)
+    start_time = attr.ib(metadata={"name": 'start-time'}, type=str, default=attr.NOTHING)
+    end_time = attr.ib(metadata={"name": 'end-time'}, type=str, default=attr.NOTHING)
+    duration = attr.ib(metadata={"name": 'duration'}, default=attr.NOTHING)
+    total = attr.ib(metadata={"name": 'total'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    passed = attr.ib(metadata={"name": 'passed'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    failed = attr.ib(metadata={"name": 'failed'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    inconclusive = attr.ib(metadata={"name": 'inconclusive'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    skipped = attr.ib(metadata={"name": 'skipped'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    asserts = attr.ib(metadata={"name": 'asserts'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    random_seed = attr.ib(metadata={"name": 'random-seed'}, type=int, validator=attr.validators.instance_of(int))
 
 
 @attr.s
 class TestFilterType(object):
+    pass
 
 
 @attr.s
 class CompositeFilterType(object):
+    pass
 
 
 @attr.s
 class ValueMatchFilterType(object):
-    re = attr.ib(type=object)
+    re = attr.ib(metadata={"name": 're'}, type=bool, default=attr.NOTHING)
 
 
 @attr.s
 class TestCaseElementType(object):
-    id = attr.ib(type=object)
-    name = attr.ib(type=object)
-    fullname = attr.ib(type=object)
-    methodname = attr.ib(type=object)
-    classname = attr.ib(type=object)
-    runstate = attr.ib()
-    seed = attr.ib(type=object)
-    result = attr.ib()
-    label = attr.ib(type=object)
-    site = attr.ib()
-    start-time = attr.ib(type=object)
-    end-time = attr.ib(type=object)
-    duration = attr.ib()
-    asserts = attr.ib()
+    id = attr.ib(metadata={"name": 'id'}, type=str, validator=attr.validators.instance_of(str))
+    name = attr.ib(metadata={"name": 'name'}, type=str, validator=attr.validators.instance_of(str))
+    fullname = attr.ib(metadata={"name": 'fullname'}, type=str, validator=attr.validators.instance_of(str))
+    methodname = attr.ib(metadata={"name": 'methodname'}, type=str, default=attr.NOTHING)
+    classname = attr.ib(metadata={"name": 'classname'}, type=str, default=attr.NOTHING)
+    runstate = attr.ib(metadata={"name": 'runstate'}, validator=attr.validators.instance_of(None))
+    seed = attr.ib(metadata={"name": 'seed'}, type=str, validator=attr.validators.instance_of(str))
+    result = attr.ib(metadata={"name": 'result'}, validator=attr.validators.instance_of(None))
+    label = attr.ib(metadata={"name": 'label'}, type=str, default=attr.NOTHING)
+    site = attr.ib(metadata={"name": 'site'}, default=attr.NOTHING)
+    start_time = attr.ib(metadata={"name": 'start-time'}, type=str, default=attr.NOTHING)
+    end_time = attr.ib(metadata={"name": 'end-time'}, type=str, default=attr.NOTHING)
+    duration = attr.ib(metadata={"name": 'duration'}, default=attr.NOTHING)
+    asserts = attr.ib(metadata={"name": 'asserts'}, validator=attr.validators.instance_of(NonnegativeInt32))
 
 
 @attr.s
 class TestSuiteElementType(object):
-    id = attr.ib(type=object)
-    name = attr.ib(type=object)
-    fullname = attr.ib(type=object)
-    methodname = attr.ib(type=object)
-    classname = attr.ib(type=object)
-    runstate = attr.ib()
-    type = attr.ib()
-    testcasecount = attr.ib()
-    result = attr.ib()
-    label = attr.ib(type=object)
-    site = attr.ib()
-    start-time = attr.ib(type=object)
-    end-time = attr.ib(type=object)
-    duration = attr.ib()
-    asserts = attr.ib()
-    total = attr.ib()
-    passed = attr.ib()
-    failed = attr.ib()
-    warnings = attr.ib()
-    inconclusive = attr.ib()
-    skipped = attr.ib()
+    id = attr.ib(metadata={"name": 'id'}, type=str, validator=attr.validators.instance_of(str))
+    name = attr.ib(metadata={"name": 'name'}, type=str, validator=attr.validators.instance_of(str))
+    fullname = attr.ib(metadata={"name": 'fullname'}, type=str, validator=attr.validators.instance_of(str))
+    methodname = attr.ib(metadata={"name": 'methodname'}, type=str, default=attr.NOTHING)
+    classname = attr.ib(metadata={"name": 'classname'}, type=str, default=attr.NOTHING)
+    runstate = attr.ib(metadata={"name": 'runstate'}, validator=attr.validators.instance_of(None))
+    type = attr.ib(metadata={"name": 'type'}, validator=attr.validators.instance_of(None))
+    testcasecount = attr.ib(metadata={"name": 'testcasecount'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    result = attr.ib(metadata={"name": 'result'}, validator=attr.validators.instance_of(None))
+    label = attr.ib(metadata={"name": 'label'}, type=str, default=attr.NOTHING)
+    site = attr.ib(metadata={"name": 'site'}, default=attr.NOTHING)
+    start_time = attr.ib(metadata={"name": 'start-time'}, type=str, default=attr.NOTHING)
+    end_time = attr.ib(metadata={"name": 'end-time'}, type=str, default=attr.NOTHING)
+    duration = attr.ib(metadata={"name": 'duration'}, default=attr.NOTHING)
+    asserts = attr.ib(metadata={"name": 'asserts'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    total = attr.ib(metadata={"name": 'total'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    passed = attr.ib(metadata={"name": 'passed'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    failed = attr.ib(metadata={"name": 'failed'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    warnings = attr.ib(metadata={"name": 'warnings'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    inconclusive = attr.ib(metadata={"name": 'inconclusive'}, validator=attr.validators.instance_of(NonnegativeInt32))
+    skipped = attr.ib(metadata={"name": 'skipped'}, validator=attr.validators.instance_of(NonnegativeInt32))
 
 
