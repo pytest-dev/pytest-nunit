0e5507680fc7417c934243ab259f745b9ced7274
Anthony Shaw <anthonyshaw@apache.org>
2019-07-11 13:59:17 +1000

    simplify test collection

--- pytest_nunit/plugin.py
+++ pytest_nunit/plugin.py
@@ -99,33 +99,8 @@ class _NunitNodeReporter:
 
     def record_testreport(self, testreport):
         log.debug("record_test_report:{0}".format(testreport))
-        self.nunit_xml.cases.append(testreport)
-
-    def write_captured_output(self, report):
-        if not self.nunit_xml.log_passing_tests and report.passed:
-            return
-
-        content_out = report.capstdout
-        content_log = report.caplog
-        content_err = report.capstderr
-
-    def append_pass(self, report):
-        log.debug("append_pass:{0}".format(report))
-
-    def append_failure(self, report):
-        log.debug("append_failure:{0}".format(report))
-
-    def append_collect_error(self, report):
-        log.debug("append_error:{0}".format(report))
-
-    def append_collect_skipped(self, report):
-        log.debug("append_skipped:{0}".format(report))
-
-    def append_error(self, report):
-        log.debug("append_collect_error:{0}".format(report))
-
-    def append_skipped(self, report):
-        log.debug("append_skipped:{0}".format(report))
+        if testreport.when == 'call':
+            self.nunit_xml.cases.append(testreport)
 
     def finalize(self):
         log.debug("finalize")
@@ -190,87 +165,7 @@ class NunitXML:
         return reporter
 
     def pytest_runtest_logreport(self, report):
-        """handle a setup/call/teardown report, generating the appropriate
-        xml tags as necessary.
-        note: due to plugins like xdist, this hook may be called in interlaced
-        order with reports from other nodes. for example:
-        usual call order:
-            -> setup node1
-            -> call node1
-            -> teardown node1
-            -> setup node2
-            -> call node2
-            -> teardown node2
-        possible call order in xdist:
-            -> setup node1
-            -> call node1
-            -> setup node2
-            -> call node2
-            -> teardown node2
-            -> teardown node1
-        """
-        close_report = None
-        if report.passed:
-            if report.when == "call":  # ignore setup/teardown
-                reporter = self._opentestcase(report)
-                reporter.append_pass(report)
-        elif report.failed:
-            if report.when == "teardown":
-                # The following vars are needed when xdist plugin is used
-                report_wid = getattr(report, "worker_id", None)
-                report_ii = getattr(report, "item_index", None)
-                close_report = next(
-                    (
-                        rep
-                        for rep in self.open_reports
-                        if (
-                            rep.nodeid == report.nodeid
-                            and getattr(rep, "item_index", None) == report_ii
-                            and getattr(rep, "worker_id", None) == report_wid
-                        )
-                    ),
-                    None,
-                )
-                if close_report:
-                    # We need to open new testcase in case we have failure in
-                    # call and error in teardown in order to follow junit
-                    # schema
-                    self.finalize(close_report)
-                    self.cnt_double_fail_tests += 1
-            reporter = self._opentestcase(report)
-            if report.when == "call":
-                reporter.append_failure(report)
-                self.open_reports.append(report)
-            else:
-                reporter.append_error(report)
-        elif report.skipped:
-            reporter = self._opentestcase(report)
-            reporter.append_skipped(report)
-        self.update_testcase_duration(report)
-        if report.when == "teardown":
-            reporter = self._opentestcase(report)
-            reporter.write_captured_output(report)
-
-            for propname, propvalue in report.user_properties:
-                reporter.add_property(propname, propvalue)
-
-            self.finalize(report)
-            report_wid = getattr(report, "worker_id", None)
-            report_ii = getattr(report, "item_index", None)
-            close_report = next(
-                (
-                    rep
-                    for rep in self.open_reports
-                    if (
-                        rep.nodeid == report.nodeid
-                        and getattr(rep, "item_index", None) == report_ii
-                        and getattr(rep, "worker_id", None) == report_wid
-                    )
-                ),
-                None,
-            )
-            if close_report:
-                self.open_reports.remove(close_report)
+        reporter = self._opentestcase(report)
 
     def update_testcase_duration(self, report):
         """accumulates total duration for nodeid from given report and updates
@@ -281,15 +176,11 @@ class NunitXML:
             reporter.duration += getattr(report, "duration", 0.0)
 
     def pytest_collectreport(self, report):
-        if not report.passed:
-            reporter = self._opentestcase(report)
-            if report.failed:
-                reporter.append_collect_error(report)
-            else:
-                reporter.append_collect_skipped(report)
+        pass
 
     def pytest_internalerror(self, excrepr):
         reporter = self.node_reporter("internal")
+
 
     def pytest_sessionstart(self):
         self.suite_start_time = datetime.now()
@@ -301,6 +192,10 @@ class NunitXML:
             os.makedirs(dirname)
         self.suite_stop_time = datetime.now()
         self.suite_time_delta = (self.suite_stop_time-self.suite_start_time).total_seconds()
+
+        self.stats['total'] = len(self.cases)
+        self.stats['passed'] = len(list(case for case in self.cases if case.outcome == 'passed'))
+
         with open(self.logfile, "w", encoding="utf-8") as logfile:
             logfile.write('<?xml version="1.0" encoding="utf-8"?>')
             result = NunitTestRun(self).generate_xml()
