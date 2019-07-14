52df056834be22d8b457705e2ca9af4c562456ca
Anthony Shaw <anthonyshaw@apache.org>
2019-07-14 14:34:45 +1000

    Missed join

--- tests/integration/test_azure.py
+++ tests/integration/test_azure.py
@@ -3,6 +3,6 @@ Test things in Azure Pipelines
 """
 
 def test_attachment(add_nunit_attachment):
-    pth = os.path.abspath(os.path.dirname(__file__)), 'fixture.gif')
+    pth = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fixture.gif')
     add_nunit_attachment(path, "PBJT")
     assert 1 == 1
