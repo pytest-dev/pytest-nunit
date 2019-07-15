99a3239579550e824480897f3305b6f00aa6031c
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-07-15 13:26:19 +1000

    Capture requirement for python 2.7 and 3.4 only

--- setup.py
+++ setup.py
@@ -28,11 +28,15 @@ setup(
     maintainer_email='anthonyshaw@apache.org',
     license='MIT',
     url='https://github.com/tonybaloney/pytest-nunit',
-    description='A pytest plugin for generating Nunit3 test result XML output',
+    description='A pytest plugin for generating NUnit3 test result XML output',
     long_description=read('README.rst'),
     packages=find_packages(exclude=('test*',)),
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['pytest>=3.5.0', 'enum34', 'attrs'],
+    install_requires=['pytest>=3.5.0', 'attrs'],
+    extras_require={
+        ':python_version=="2.7"': ['enum34>=1.1.6'],
+        ':python_version=="3.4"': ['enum34>=1.1.6'],
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Pytest',
