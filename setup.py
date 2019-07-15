d9253c4377a12c0602ddfae2ac1a61cfeb2be160
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-07-15 13:17:05 +1000

    Unpin enum34

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     long_description=read('README.rst'),
     packages=find_packages(exclude=('test*',)),
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['pytest>=3.5.0', 'enum34==1.1.6', 'attrs'],
+    install_requires=['pytest>=3.5.0', 'enum34', 'attrs'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Pytest',
